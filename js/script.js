06102cc259a7c44d99e92acd50290d3bf9705297
shaheerali838 <shaheer838838@gmail.com>
2025-01-13 14:24:37 +0500

    enhance animations and styling for header, home section, and about section

--- js/script.js
+++ js/script.js
@@ -26,11 +26,16 @@ window.onscroll = () => {
           .querySelector(`header nav a[href*=` + id + `]`)
           .classList.add("active");
       });
+
+      // active section for animation on scroll
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
     }
-    //   sticky header
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 100);
   });
+  //   sticky header
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
   // remove toggle icon and navbar when click on link
   menuIcon.classList.remove("bx-x");
